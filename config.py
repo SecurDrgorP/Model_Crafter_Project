77723b1007453d45bddd0e375931d3ccf878b973
mouhsiiin <boumargoud4mouhsin@gmail.com>
2025-02-08 14:39:32 +0000

    Update .gitignore and enhance config for directory creation and parameter adjustments

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import logging
+import os
+
 
 # Base directory
 BASE_DIR = Path(__file__).parent
@@ -10,10 +12,21 @@ SPLIT_DATA_DIR = BASE_DIR / "data" / "split_dataset"
 MODEL_DIR = BASE_DIR / "models"
 RESULTS_DIR = BASE_DIR / "results"
 
+
+if not os.path.exists(SPLIT_DATA_DIR):
+    os.makedirs(SPLIT_DATA_DIR)
+
+if not os.path.exists(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
+
+if not os.path.exists(RESULTS_DIR):
+    os.makedirs(RESULTS_DIR)
+    
+
 # Model parameters
 BATCH_SIZE = 16 # Number of images to process at once before updating weights and biases in the model data file ( defaults to 32) 
-IMG_SIZE = (128, 128) # Image size for resizing the images to a consistent size ( defaults to (256, 256) )
-EPOCHS = 10 # Number of times the model will cycle through the entire dataset ( defaults to 20 )
+IMG_SIZE = (256, 256) # Image size for resizing the images to a consistent size ( defaults to (256, 256) )
+EPOCHS = 17 # Number of times the model will cycle through the entire dataset ( defaults to 20 )
 # Random seed for reproducibility
 SEED = 42 # Seed for random number generation ( defaults to 42 ) ; default value is used for reproducibility
 
